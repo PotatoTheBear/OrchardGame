62e341e7612dfc83992651c008892561b315cbaf
Hey-Thing <159765239+Hey-Thing@users.noreply.github.com>
2026-05-01 16:02:38 +0200

    added getClosest back in

--- Assets/Scripts/Fruit Trees/FruitTree.cs
+++ Assets/Scripts/Fruit Trees/FruitTree.cs
@@ -83,4 +83,22 @@ public abstract class FruitTree : MonoBehaviour
             rb.linearVelocity = randDirection;
         }
     }
+
+    public static FruitTree GetClosest(Vector2 position, float minDist = Mathf.Infinity)
+    {
+        FruitTree closest = null;
+
+        foreach (FruitTree fruitTree in All)
+        {
+            float distance = (new Vector2(fruitTree.transform.position.x, fruitTree.transform.position.y) - position).sqrMagnitude;
+
+            if (distance < minDist)
+            {
+                closest = fruitTree;
+                minDist = distance;
+            }
+        }
+
+        return closest;
+    }
 }
