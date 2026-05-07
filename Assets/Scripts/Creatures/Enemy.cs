1f2f1b4164f7b5b1062acd4b1cd43991baa35fe2
Hedin <hmseinen@st.noorderpoort.nl>
2026-05-07 09:31:46 +0200

    ant gun works

--- Assets/Scripts/Creatures/Enemy.cs
+++ Assets/Scripts/Creatures/Enemy.cs
@@ -11,12 +11,25 @@ public class Enemy : Creature
 {
     private IEnemyMovement movement;
     private IAim aim;
+    private GameObject handleGameObject;
+    private Weapons[] weaponsCache;
+    [SerializeField] private bool attackAllWeapons = true;
     [SerializeField] private EnemyBehavior behavior = EnemyBehavior.CloserTarget;
 
     private void Start()
     {
         movement = new EntityMovement();
         aim = new AutoAim();
+        var handle = transform.Find("Handle");
+        if (handle != null)
+        {
+            handleGameObject = handle.gameObject;
+            weaponsCache = handleGameObject.GetComponentsInChildren<Weapons>();
+        }
+        else
+        {
+            weaponsCache = new Weapons[0];
+        }
     }
 
     void Update()
@@ -48,14 +61,35 @@ public class Enemy : Creature
                 var treePos = closestTree.transform.position;
                 treeSqr = (new Vector2(treePos.x, treePos.y) - enemyPos2).sqrMagnitude;
             }
-
+            // if the player is closer than the closest tree, move towards the player, otherwise move towards the tree
             if (playerSqr <= treeSqr)
                 movement.MoveToPlayer(this);
             else
                 movement.MoveToTree(this);
         }
-        aim.Aim(false, transform.Find("Handle").gameObject);
-        transform.Find("Handle").Find("TestEnemyGun").gameObject.GetComponent<Weapons>().Attack(transform.position, transform.rotation.eulerAngles.z, false);
+        // Aim using cached handle if available
+        if (handleGameObject != null)
+            aim.Aim(false, handleGameObject);
+        else
+            aim.Aim(false, gameObject);
+
+        var pos = transform.position;
+        var rotZ = transform.rotation.eulerAngles.z;
+
+        // Attack with weapons (mode controlled by attackAllWeapons)
+        if (weaponsCache != null && weaponsCache.Length > 0)
+        {
+            if (attackAllWeapons)
+            {
+                foreach (var w in weaponsCache)
+                    w?.Attack(pos, rotZ, false);
+            }
+            else
+            {
+                // only use the first weapon (order comes from GetComponentsInChildren)
+                weaponsCache[0]?.Attack(pos, rotZ, false);
+            }
+        }
         Death();
     }
 }
