9c0713029a2f0affdb7a795709840a2b5400614b
Zoe Ladino <zoeladino@hotmail.com>
2026-05-08 14:02:59 +0200

    Arcade select
    Er is een extra image toegevoegd, waardoor je nu in de main menu de optie hebt om te kiezen welke controls je wilt gebruiken.

--- Assets/Scripts/MainMenu/MainMenuScript.cs
+++ Assets/Scripts/MainMenu/MainMenuScript.cs
@@ -1,10 +1,36 @@
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UIElements;
+using UnityEngine.EventSystems;
 public class MainMenuScript : MonoBehaviour
 {
-  
+
+    public GameObject imagePlay;
+    public GameObject imageControlSelect;
+
+    public GameObject playButton;
+
+    private void Start()
+    {
+        imagePlay.SetActive(false);
+        imageControlSelect.SetActive(true);
+    }
+    public void OnArcadePress()
+    {
+        imagePlay.SetActive(true);
+        imageControlSelect.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(playButton);
+    }
+
+    public void OnPCPress()
+    {
+        imagePlay.SetActive(true);
+        imageControlSelect.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(playButton);
+    }
     public void OnPlayPress()
     {
         SceneManager.LoadScene("DeBoomgaarde");
